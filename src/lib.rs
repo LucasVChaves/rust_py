581138e48557910a802eb368d9c61cabe44c236f
Lucas Chaves <lucasvchaves@gmail.com>
2023-11-07 19:15:57 -0300

    Changed the boilerplate code for a word counter function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
 use pyo3::prelude::*;
+use std::collections::HashMap;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn count_words(s: String) -> Py<PyAny> {
+    let mut hm = HashMap::new();
+    for sub in s.split(' '){
+        let count = hm
+            .entry(sub)
+            .or_insert(0);
+        *count += 1;
+    }
+    
+    return Python::with_gil(|py| {
+        hm.to_object(py)
+    });
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rust_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(count_words, m)?)?;
     Ok(())
 }
